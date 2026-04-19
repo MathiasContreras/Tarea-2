5541d0f4409b40dfdd7ce7afd6c2ccdd68d2879e
Mathias Contreras Zaldumbide <mathias.contreras@udla.edu.ec>
2026-04-18 19:54:06 -0500

    Declaracion de variables

---
+++ Tarea2.c
@@ -0,0 +1,10 @@
+int main() {
+    int num_estudiantes = 5;
+    int num_asignaturas = 3;
+    
+    float calificaciones[5][3]; 
+    
+    float suma_estudiante, suma_asignatura;
+    float max_est, min_est, max_asig, min_asig;
+    int aprobados, reprobados;
+}
