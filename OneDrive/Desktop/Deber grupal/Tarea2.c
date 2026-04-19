0ffe418d0825298bb4f53d31abcc63e8369e40de
Juan David <juan.cartagena.geate@udla.edu.ec>
2026-04-18 20:29:30 -0500

    Validacion del rango de notas

--- OneDrive/Desktop/Deber grupal/Tarea2.c
+++ OneDrive/Desktop/Deber grupal/Tarea2.c
@@ -8,3 +8,20 @@ int main() {
     float max_est, min_est, max_asig, min_asig;
     int aprobados, reprobados;
 }
+
+for (int i = 0; i < num_estudiantes; i++) {
+        printf("\n Notas del Estudiante %d \n", i + 1);
+        for (int j = 0; j < num_asignaturas; j++) {
+           
+            do {
+                printf("Ingrese la calificacion para la asignatura %d (0 a 10): ", j + 1);
+                scanf("%f", &calificaciones[i][j]);
+                
+                if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10) {
+                    printf("Error: La calificacion debe estar en el rango de 0 a 10.\n");
+                }
+            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+        }
+    }
+
+    
